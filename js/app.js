b16fbd1867c25ef482ff4a9dcac44f5182db6703
Mangesh2042 <mangesh2042@gmail.com>
2022-03-02 13:05:00 +0530

    scroller added image size fixed

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
- 
+$(document).ready(function()
+{
+    $('.load_slider1').slick({
+        autoplay:true,
+        slidesToShow:3,
+        slidesToScroll:1,
+        prevArrow:".prev-btn1",
+        nextArrow:".next-btn1"
+    })
+})
+
+
+$(document).ready(function()
+{
+    $('.load_slider2').slick({
+        autoplay:true,
+        slidesToShow:3,
+        slidesToScroll:1,
+        prevArrow:".prev-btn2",
+        nextArrow:".next-btn2"
+    })
+})
+
+$(document).ready(function()
+{
+    $('.load_slider3').slick({
+        autoplay:true,
+        slidesToShow:3,
+        slidesToScroll:1,
+        prevArrow:".prev-btn3",
+        nextArrow:".next-btn3"
+    })
+})
+
---
+++ js/home.js
@@ -0,0 +1,218 @@
+var list1=
+[
+    {id:1,tag:"BEST SELLER",image_src:"../images/product1.jpg",para:"ramdon text",rate:"4.9",
+    star_url:"../icons/star-16.png",price:"$399"},
+
+    {id:2,tag:"BEST SELLER",image_src:"../images/product1.jpg",para:"ramdon text",rate:"4.9",
+    star_url:"../icons/star-16.png",price:"$399"},
+
+    {id:3,tag:"BEST SELLER",image_src:"../images/product1.jpg",para:"ramdon text",rate:"4.9",
+    star_url:"../icons/star-16.png",price:"$399"},
+
+    {id:4,tag:"BEST SELLER",image_src:"../images/product1.jpg",para:"ramdon text",rate:"4.9",
+    star_url:"../icons/star-16.png",price:"$399"},
+
+    {id:5,tag:"BEST SELLER",image_src:"../images/product1.jpg",para:"ramdon text",rate:"4.9",
+    star_url:"../icons/star-16.png",price:"$399"}
+]
+
+
+
+slider_list_1(list1)
+slider_list_2(list1)
+slider_list_3(list1)
+
+
+function slider_list_1(list1)
+{
+    list1.forEach(element => {
+        createitems1(element)
+       
+    });
+   
+}
+
+function slider_list_2(list1)
+{
+    list1.forEach(element => {
+        createitems2(element)
+     
+    });
+   
+}
+
+function slider_list_3(list1)
+{
+    list1.forEach(element => {
+        createitems3(element)
+     
+    });
+   
+}
+
+
+function createitems1(element)
+{
+     let slider1=document.getElementById("slider1")
+   
+    let product_cart=document.createElement("div")
+    product_cart.className="products food-card .magic-shadow-sm"
+    product_cart.id="product1"
+     
+    let best_seller_tag=document.createElement("div")
+    best_seller_tag.className="best_seller_tag"
+    best_seller_tag.innerText=element.tag
+
+
+   let product_images=document.createElement("img")
+   product_images.src=element.product_images
+   product_images.className="product_images"
+   product_images.src=element.image_src
+
+   let para=document.createElement("p")
+   para.className="para"
+   para.innerText=element.para;
+
+
+   let star=document.createElement("div")
+   star.className="star"
+
+   let flex=document.createElement("div")
+   flex.className="flex"
+
+   let rate=document.createElement("p")
+   rate.className="rate"
+   rate.innerText=element.rate
+
+
+   let star_icon=document.createElement("img")
+   star_icon.className="star_icon"
+   star_icon.src=element.star_url
+   
+   flex.append(rate,star_icon)
+   star.append(flex)
+
+   let h4=document.createElement("h4")
+   h4.innerText=element.price
+
+   let add_card=document.createElement("div")
+   add_card.className="add_card"
+   add_card.innerText=" ADD TO CARD"
+
+   product_cart.append(best_seller_tag,product_images,para,star,h4,add_card)
+   slider1.append(product_cart) 
+
+   console.log(product_cart)
+}
+
+function createitems2(element)
+{
+     let slider1=document.getElementById("slider2")
+   
+    let product_cart=document.createElement("div")
+    product_cart.className="products food-card .magic-shadow-sm"
+    product_cart.id="product1"
+     
+    let best_seller_tag=document.createElement("div")
+    best_seller_tag.className="best_seller_tag"
+    best_seller_tag.innerText=element.tag
+
+
+   let product_images=document.createElement("img")
+   product_images.src=element.product_images
+   product_images.className="product_images"
+   product_images.src=element.image_src
+
+   let para=document.createElement("p")
+   para.className="para"
+   para.innerText=element.para;
+
+
+   let star=document.createElement("div")
+   star.className="star"
+
+   let flex=document.createElement("div")
+   flex.className="flex"
+
+   let rate=document.createElement("p")
+   rate.className="rate"
+   rate.innerText=element.rate
+
+
+   let star_icon=document.createElement("img")
+   star_icon.className="star_icon"
+   star_icon.src=element.star_url
+   
+   flex.append(rate,star_icon)
+   star.append(flex)
+
+   let h4=document.createElement("h4")
+   h4.innerText=element.price
+
+   let add_card=document.createElement("div")
+   add_card.className="add_card"
+   add_card.innerText=" ADD TO CARD"
+
+   product_cart.append(best_seller_tag,product_images,para,star,h4,add_card)
+   slider1.append(product_cart) 
+
+   console.log(product_cart)
+}
+
+function createitems3(element)
+{
+     let slider1=document.getElementById("slider3")
+   
+    let product_cart=document.createElement("div")
+    product_cart.className="products food-card .magic-shadow-sm"
+    product_cart.id="product1"
+     
+    let best_seller_tag=document.createElement("div")
+    best_seller_tag.className="best_seller_tag"
+    best_seller_tag.innerText=element.tag
+
+
+   let product_images=document.createElement("img")
+   product_images.src=element.product_images
+   product_images.className="product_images"
+   product_images.src=element.image_src
+
+   let para=document.createElement("p")
+   para.className="para"
+   para.innerText=element.para;
+
+
+   let star=document.createElement("div")
+   star.className="star"
+
+   let flex=document.createElement("div")
+   flex.className="flex"
+
+   let rate=document.createElement("p")
+   rate.className="rate"
+   rate.innerText=element.rate
+
+
+   let star_icon=document.createElement("img")
+   star_icon.className="star_icon"
+   star_icon.src=element.star_url
+   
+   flex.append(rate,star_icon)
+   star.append(flex)
+
+   let h4=document.createElement("h4")
+   h4.innerText=element.price
+
+   let add_card=document.createElement("div")
+   add_card.className="add_card"
+   add_card.innerText=" ADD TO CARD"
+
+   product_cart.append(best_seller_tag,product_images,para,star,h4,add_card)
+   slider1.append(product_cart) 
+
+   console.log(product_cart)
+}
+
+ 
+ 
+ 
