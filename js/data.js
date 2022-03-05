7b8062ad22624ab56f0e1489c48a1d7e05af2982
raju89 <raju199495@gmail.com>
2022-03-05 12:40:06 +0530

    js file add

---
+++ js/data.js
@@ -0,0 +1,63 @@
+var list1=
+[
+    {id:1,p_name:" Apple Body Wash ",tag:"BEST SELLER",image_src:"../image/1.jpg",para:"Hi! I am Mamaearth Agent Apple Body Wash for your little bundle of joy. ",rate:"4.9",
+    star_url:"../icons/star-16.png",price:"299",price_old:"400",symbol:"₹",off:"30%"},
+
+    {id:2,p_name:"mango-body-wash ",tag:"BEST SELLER",image_src:"../image/2.jpg",para:"Hi! I am Mamaearth Major Mango Body Wash for your little bundle of joy.",rate:"4.9",
+    star_url:"../icons/star-16.png",price:"199",price_old:"299",symbol:"₹",off:"30%"},
+
+    {id:3,p_name:" Strawberry Body Lotion for Kids ",tag:"BEST SELLER",image_src:"../image/3.jpg",para:"Hi, I am Mamaearth Super Strawberry Body Lotion crafted especially for your li’l one",rate:"4.9",
+    star_url:"../icons/star-16.png",price:"399",price_old:"490",symbol:"₹",off:"30%"},
+
+    {id:4,p_name:" Orange Body Lotion For Kids ",tag:"BEST SELLER",image_src:"../image/4.jpg",para:" Hi! I am Mamaearth Original Orange Body Lotion! Crafted with the natural goodness of Orange and Shea butter ",rate:"4.9",
+    star_url:"../icons/star-16.png",price:"299",price_old:"400",symbol:"₹",off:"30%"},
+
+    {id:5,p_name:" major-mango-body-lotion-for-kids ",tag:"BEST SELLER",image_src:"../image/5.jpg",para:" Hi! I am Mamaearth Major Mango Body Lotion, I am here to protect and nourish your munchkin’s delicate skin. ",rate:"4.9",
+    star_url:"../icons/star-16.png",price:"199",price_old:"400",symbol:"₹",off:"30%"},
+
+    {id:6,p_name:" agent-apple-body-lotion ",tag:"BEST SELLER",image_src:"../image/6.jpg",para:"Hi! I am Mamaearth Agent Apple Body Wash for your little bundle of joy. ",rate:"4.9",
+    star_url:"../icons/star-16.png",price:"299",price_old:"450",symbol:"₹",off:"30%"},
+
+    {id:7,p_name:" Nourishing Hair Oil for babies ",tag:"BEST SELLER",image_src:"../image/7.jpg",para:" Hi! I am Mamaearth Nourishing Baby Hair Oil. I am non-sticky, mild and gentle on baby’s scalp.",rate:"4.9",
+    star_url:"../icons/star-16.png",price:"299",price_old:"399",symbol:"₹",off:"30%"},
+
+    {id:8,p_name:"baby-wipes-72pc ",tag:"BEST SELLER",image_src:"../image/8.jpg",para:"Hi, I am Mamaearth Bamboo Based Baby Wipes for your baby’s everyday needs. ",rate:"4.9",
+    star_url:"../icons/star-16.png",price:"299",price_old:"300",symbol:"₹",off:"30%"},
+
+    {id:9,p_name:"welcome-baby-essential-kit ",tag:"BEST SELLER",image_src:"../image/9.jpg",para: "Hi! I am the Welcome Baby Essential Kit and I come with laundry detergent, moisturizing soap bars,",rate:"4.9",
+    star_url:"../icons/star-16.png",price:"99",price_old:"400",symbol:"₹",off:"30%"},
+
+    {id:10,p_name:" dusting-powder ",tag:"BEST SELLER",image_src:"../image/10.jpg",para:"Hi! I am Mamaearth Dusting Powder for Babies. I am dermatologically tested, 100% natural, non-lung-clogging & talc-free baby powder for rash.",rate:"4.9",
+    star_url:"../icons/star-16.png",price:"99",price_old:"200",symbol:"₹",off:"30%"},
+
+    {id:11,p_name:" Gentle Cleansing Shampoo",tag:"BEST SELLER",image_src:"../image/11.jpg",para:"Hi! I am Mamaearth Gentle Cleansing Shampoo. I understand that shampooing is not an easy task and involves effort in real cleaning.  ",rate:"4.9",
+    star_url:"../icons/star-16.png",price:"399",price_old:"500",symbol:"₹",off:"30%"},
+
+    {id:12,p_name:"Baby Essential Hamper Kit ",tag:"BEST SELLER",image_src:"../image/12.jpg",para:" Hi! I am the Baby Essential Hamper Kit and I can’t wait to come on this parenting journey with you. I come prepared with everything you need. ",rate:"4.9",
+    star_url:"../icons/star-16.png",price:"99",price_old:"100",symbol:"₹",off:"30%"},
+
+    {id:13,p_name:" Foaming Facewash for Kids ",tag:"BEST SELLER",image_src:"../image/13.jpg",para:"Hi! I am Mamaearth Foaming Face Wash for Kids. Dermatologically tested, and pH balanced my Tear-free and Soap-free formula cleanses and moisturizes your baby’s face gently. Infused with Coconut-based Cleansers.",rate:"4.9",
+    star_url:"../icons/star-16.png",price:"499",price_old:"600",symbol:"₹",off:"30%"},
+
+    {id:14,p_name:"Winter Essential Kit for Babies ",tag:"BEST SELLER",image_src:"../image/14.jpg",para:" Winters mean a little extra dose of nourishment and care for your little one. Confused as to where to start? Do not worry! I am the Winter Essential Kit For Babies and I have got everything you need for your child.",rate:"4.9",
+    star_url:"../icons/star-16.png",price:"999",price_old:"2000",symbol:"₹",off:"30%"},
+
+    {id:15,p_name:"Sulfate Free Awesome Orange Toothpaste",tag:"BEST SELLER",image_src:"../image/15.jpg",para:"Hi! I am Mamaearth's awesome orange toothpaste. I love to see your baby smile. ",rate:"4.9",
+    star_url:"../icons/star-16.png",price:"299",price_old:"400",symbol:"₹",off:"30%"},
+
+    {id:16,p_name:"Mineral Based Sunscreen",tag:"BEST SELLER",image_src:"../image/16.jpg",para:" Exploring their environment is the first way how a baby is able to understand its surroundings.",rate:"4.9",
+    star_url:"../icons/star-16.png",price:"99",price_old:"200",symbol:"₹",off:"30%"},
+
+    {id:17,p_name:"Soothing Massage Oil ",tag:"BEST SELLER",image_src:"../image/17.jpg",para:"Massaging your baby is a great way to help them gain mobility faster.",rate:"4.9",
+    star_url:"../icons/star-16.png",price:"299",price_old:"400",symbol:"₹",off:"30%"},
+
+    {id:18,p_name:"Milky Soft Face Cream ",tag:"BEST SELLER",image_src:"../image/18.jpg",para: "Hi! I am Mamaearth's Milky Soft Face Cream. With the goodness of Almond Oil and Murumuru Butter",rate:"4.9",
+    star_url:"../icons/star-16.png",price:"599",price_old:"800",symbol:"₹",off:"30%"},
+
+    {id:19,p_name:"Deeply Nourishing Body Wash",tag:"BEST SELLER",image_src:"../image/19.jpg",para:"Bath time is a fun time with lots of bubbles around and water splashing over the tub.",rate:"4.9",
+    star_url:"../icons/star-16.png",price:"499",price_old:"600",symbol:"₹",off:"30%"},
+
+    {id:20,p_name:"Mamaearth Fruit Punch Toothpaste",tag:"BEST SELLER",image_src:"../image/20.jpg",para:"Hi! I am Mamaearth Fruit Punch Toothpaste. I love to see your kids smile.",rate:"4.9",
+    star_url:"../icons/star-16.png",price:"99",price_old:"200",symbol:"₹",off:"30%"},
+
+]
